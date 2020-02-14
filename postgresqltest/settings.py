6f5c767c4cc9eecd472119a068bfc50b7673cd99
Rodrigo E. Gimenez <rodrigog83@gmail.com>
2020-02-14 13:06:32 +0100

    Add database information

--- postgresqltest/settings.py
+++ postgresqltest/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'postgresqltest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresqltest',
+        'USER': 'postgresqltest',
+        'PASSWORD': 'lalala123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
