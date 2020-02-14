157ec61295869221a735dfe39b93d61b532ee16f
Rodrigo E. Gimenez <rodrigog83@gmail.com>
2020-02-14 13:27:42 +0100

    Create Client model

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here
+# Codice Cliente (id),
+# Codice Fiscale,
+# Ragione Sociale/Cognome,
+# Nome,
+# Indirizzo,
+# Cap,
+# Comune,
+# Provincia,
+# Codice Stato (IT),
+# Email
+
+class Client(models.Model):
+    id = models.IntegerField(primary_key=True)
+    codice_fiscale = models.CharField(max_length=16)
+    ragione_cognome = models.CharField(max_length=128)
+    nome = models.CharField(max_length=64)
+    indirizzo = models.CharField(max_length=128)
+    cap = models.IntegerField()
+    comune = models.CharField(max_length=64)
+    provincia = models.CharField(max_length=64)
+    stato = models.CharField(max_length=2)
+    email = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.id}:{self.codice_fiscale}, {self.nome}"
